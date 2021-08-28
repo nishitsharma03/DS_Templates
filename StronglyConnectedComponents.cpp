dd5d5a27475e0ebca8c896a1d33a7600a047a375
Nishit Sharma <nishitsharma0@gmail.com>
2021-08-28 15:40:08 +0530

    Update StronglyConnectedComponents.cpp

--- StronglyConnectedComponents.cpp
+++ StronglyConnectedComponents.cpp
@@ -1,14 +1,19 @@
+// Problem: CSES coin collector https://cses.fi/problemset/task/1686/
 #include<bits/stdc++.h>
-#define ll long long int
+#define endl "\n"
 #define quick ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
 
 using namespace std;
 
+#define int long long int
 class SCC
 {
 public:
-    vector<vector<int>> v, vRev, components;
+    vector<vector<int>> v, vRev, components, condV;
+    vector<int> compValue, nodeValue;
     int n;
+
+    // Constructor 1
     SCC(int n)
     {
         this->n = n;
@@ -16,6 +21,31 @@ public:
         vRev.resize(n);
     }
 
+    // Constructor 2 (number of nodes, value of nodes)
+    SCC(int n, vector<int> &a)
+    {
+        this->n = n;
+        v.resize(n);
+        vRev.resize(n);
+        nodeValue = a;
+    }
+
+    // Constructor 2(adjacency list)
+    SCC(vector<vector<int>> &adj)
+    {
+        v = adj;
+        this->n = adj.size();
+        vRev.resize(n);
+        for (int i = 0; i < n; i++)
+        {
+            for (int &j : adj[i])
+            {
+                vRev[j].push_back(i);
+            }
+        }
+    }
+
+    // 0-based indexing
     void insertEdge(int a, int b)
     {
         v[a].push_back(b);
@@ -45,6 +75,8 @@ public:
         order.push_back(src);
     }
 
+
+    // 0- based indexing
     void getSCC()
     {
         vector<int> vis(n);
@@ -56,7 +88,7 @@ public:
                 dfs(i, vis, order);
             }
         }
-        assert(int(order.size()) == n);
+        assert(order.size() == n);
         fill(vis.begin(), vis.end(), 0);
         for (int i = n - 1; i >= 0; i--)
         {
@@ -71,40 +103,134 @@ public:
     }
 
 
+
+    void getCondensedGraph()
+    {
+        int connComps = this->components.size();
+        condV.resize(connComps);
+        compValue.resize(connComps);
+        vector<int> comp(n);
+        for (int i = 0; i < this->components.size(); i++)
+        {
+            for (int &j : this->components[i])
+            {
+                comp[j] = i;
+                compValue[i] += nodeValue[j];
+            }
+        }
+
+        vector<int> used(connComps);
+
+        for (int i = 0; i < this->components.size(); i++)
+        {
+            for (int &j : this->components[i])
+            {
+
+                for (int &k : this->v[j])
+                {
+                    int otherComp = comp[k];
+
+                    if (!used[otherComp] and otherComp != i)
+                    {
+                        condV[i].push_back(otherComp);
+                        used[otherComp] = 1;
+                    }
+
+                }
+            }
+            for (int &j : this->components[i])
+            {
+
+                for (int &k : this->v[j])
+                {
+                    int otherComp = comp[k];
+                    used[otherComp] = 0;
+
+                }
+            }
+        }
+
+    }
+
+
 };
 
 
-int main()
+int32_t main()
 {
     quick;
     int n, m;
     cin >> n >> m;
-    SCC scc(n);
+
+    vector<int> a(n);
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
+
+    SCC scc(n, a);
     for (int i = 0; i < m; i++)
     {
         int a, b;
         cin >> a >> b;
-        a--, b--;
-        scc.insertEdge(a, b);
+        scc.insertEdge(a - 1, b - 1);
     }
+
 
     scc.getSCC();
+    scc.getCondensedGraph();
 
-    if (scc.components.size() == 1)
+
+    int nNew = scc.condV.size();
+
+    vector<int> inDeg(nNew);
+
+    for (auto &i : scc.condV)
     {
-        cout << "YES" << endl;
+        for (auto &j : i)
+        {
+            inDeg[j]++;
+        }
+
     }
-    else
+
+    vector<int> dp(nNew);
+    queue<int> q;
+
+    for (int i = 0; i < nNew; i++)
     {
-        assert(scc.components.size() > 1);
-        cout << "NO" << endl;
-        vector<int> vis(n), order;
-        int u = scc.components[0][0], v = scc.components[1][0];
-        scc.dfs(u, vis, order);
-        if (vis[v])
-            swap(u, v);
-        cout << u + 1 << " " << v + 1 << endl;
+        dp[i] = scc.compValue[i];
+
+        if (inDeg[i] == 0)
+            q.push(i);
     }
+
+
+    while (q.empty() == false)
+    {
+        int p = q.front();
+        q.pop();
+
+        for (int &i : scc.condV[p])
+        {
+            dp[i] = max(dp[i], dp[p] + scc.compValue[i]);
+            inDeg[i]--;
+            if (inDeg[i] == 0)
+                q.push(i);
+        }
+    }
+
+
+    int ans = 0;
+    for (int &i : dp)
+        ans = max(ans, i);
+
+    cout << ans << endl;
+
+
+
+
+
 
     cerr << "time taken : " << (float)clock() / CLOCKS_PER_SEC << " secs" << endl;
     return 0;
