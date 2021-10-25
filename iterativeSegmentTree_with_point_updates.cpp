357cff475b62170aafc6c5daf4bf9393462088f5
Nishit Sharma <nishitsharma0@gmail.com>
2021-10-26 03:37:35 +0530

    Update iterativeSegmentTree_with_point_updates.cpp

--- iterativeSegmentTree_with_point_updates.cpp
+++ iterativeSegmentTree_with_point_updates.cpp
@@ -5,67 +5,72 @@ using namespace std;
 class segtree
 {
 public:
-    vector<int> seg;
-    vector<int> a;
-    int n;
-    segtree(vector<int> &v)
-    {
-        n = v.size();
-        a = v;
-        seg.resize(2 * n);
-    }
+	vector<int> seg;
+	vector<int> a;
+	int n;
+	int placeHolder;
+	segtree(vector<int> &v)
+	{
+		n = v.size();
+		a = v;
+		placeHolder = 0;
+		seg.resize(2 * n);
+	}
 
+	int merge(int a, int b)
+	{
+		return (a + b);
+	}
 
 //!-------- CAUTION this Tree works for [l,r) .... mind the open interval on r -------!
-    void build( )
-    {
-        for (int i = 0; i < n; i++)
-        {
-            seg[i + n] = a[i];
-        }
-        for ( int i = n - 1; i > 0; i--)
-        {
-            seg[i] = max(seg[2 * i] , seg[2 * i + 1]);
-        }
+	void build( )
+	{
+		for (int i = 0; i < n; i++)
+		{
+			seg[i + n] = a[i];
+		}
+		for ( int i = n - 1; i > 0; i--)
+		{
+			seg[i] = merge(seg[2 * i] , seg[2 * i + 1]);
+		}
 
-    }
+	}
 
-    void update( int ind , int val)
-    {
-        a[ind] = val;
-        ind += n;
-        seg[ind] = val;
+	void update( int ind , int val)
+	{
+		a[ind] = val;
+		ind += n;
+		seg[ind] = val;
 
-        for ( ; ind > 1; ind >>= 1)
-        {
-            seg[ind >> 1] = max(seg[ind] , seg[ind ^ 1]);
-        }
-    }
+		for ( ; ind > 1; ind >>= 1)
+		{
+			seg[ind >> 1] = merge(seg[ind] , seg[ind ^ 1]);
+		}
+	}
 //!-------- CAUTION this function works for [l,r) .... mind the open interval on r -------!
-    int query(int l, int r)
-    {
-        l += n;
-        r += n;
-        int ans = -1e9;
-        while (l < r)
-        {
-            if (l % 2)
-            {
-                ans = max(ans, seg[l]);
-                l++;
-            }
-            if (r % 2)
-            {
-                --r;
-                ans =  max(ans, seg[r]);
-            }
-            l >>= 1;
-            r >>= 1;
-        }
+	int query(int l, int r)
+	{
+		l += n;
+		r += n;
+		int ans = placeHolder;
+		while (l < r)
+		{
+			if (l % 2)
+			{
+				ans = merge(ans, seg[l]);
+				l++;
+			}
+			if (r % 2)
+			{
+				--r;
+				ans =  merge(ans, seg[r]);
+			}
+			l >>= 1;
+			r >>= 1;
+		}
+		return ans;
 
-        return ans;
-
-    }
+	}
 
 };
 
